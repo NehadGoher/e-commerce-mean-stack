9c3910032a0df19080f82a18949aef555a00f501
NehadGoher <nehad.goher@gmail.com>
2019-04-27 01:19:01 +0200

    Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterCComponent } from './footer-c/footer-c.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterCComponent
+    FooterCComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
