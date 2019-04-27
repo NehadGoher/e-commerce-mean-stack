0a82e64ce434e200a542e0db42859f9be9883b47
Basmae <basma_essam_1@yahoo.com>
2019-04-28 00:36:44 +0200

    Add files via upload

---
+++ src/app/product.ts
@@ -0,0 +1,12 @@
+export default class Product {
+    ProductName: String;
+    SupplierID: Number;
+    CategoryID:Number;
+    QuantityPerUnit:String;
+    UnitPrice:Number;
+    UnitsInStock:Number;
+    UnitsOnOrder:Number;
+    ReorderLevel:Number;
+    Discontinued:Number;
+
+  }
